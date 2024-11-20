8520db69b918e09b96925d615d18d3757957e2ba
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-11-20 00:15:10 -0500

    WIP

---
+++ spCoin_TS_Methods.js
@@ -0,0 +1,46 @@
+const { SpCoinLogger, LOG_MODE } = require("./lib/utils/logging");
+const { SpCoinERC20Methods } = require("./lib/spCoinTransferMethods");
+const { SpCoinDeleteMethods } = require("./lib/spCoinDeleteMethods");
+const { SpCoinAddMethods } = require("./lib/spCoinAddMethods");
+const { SpCoinReadMethods } = require("./lib/SpCoinReadMethods");
+const { SpCoinRewardsMethods } = require("./lib/spCoinRewardsMethods"); 
+const { SpCoinStakingMethods } = require("./lib/spCoinStakingMethods"); 
+const { second, minute, hour, day, week, year, month , millennium } = require("./lib/spCoinStakingMethods");
+
+
+class SpCoinClassMethods {
+  constructor(_spCoinContractDeployed) {
+    this.spCoinContractDeployed = _spCoinContractDeployed;
+    this.spCoinAddMethods = new SpCoinAddMethods(this.spCoinContractDeployed);
+    this.spCoinDeleteMethods = new SpCoinDeleteMethods(this.spCoinContractDeployed);
+    this.spCoinERC20Methods = new SpCoinERC20Methods(this.spCoinContractDeployed);
+    this.spCoinLogger = new SpCoinLogger(this.spCoinContractDeployed);
+    this.spCoinReadMethods = new SpCoinReadMethods(this.spCoinContractDeployed);
+    this.spCoinRewardsMethods = new SpCoinRewardsMethods(this.spCoinContractDeployed);
+    this.spCoinStakingMethods = new SpCoinStakingMethods(this.spCoinContractDeployed);
+  }
+
+  methods = () => {
+    return {
+      spCoinContractDeployed : this.spCoinContractDeployed,
+      spCoinAddMethods       : this.spCoinAddMethods,
+      spCoinDeleteMethods    : this.spCoinDeleteMethods,
+      spCoinERC20Methods     : this.spCoinERC20Methods,
+      spCoinLogger           : this.spCoinLogger,
+      spCoinReadMethods      : this.spCoinReadMethods,
+      spCoinRewardsMethods   : this.spCoinRewardsMethods,
+      spCoinStakingMethods   : this.spCoinStakingMethods
+    }
+  }
+}
+
+module.exports = {
+  SpCoinClassMethods,
+  SpCoinAddMethods,
+  SpCoinDeleteMethods,
+  SpCoinERC20Methods,
+  SpCoinLogger,
+  SpCoinReadMethods,
+  SpCoinRewardsMethods,
+  SpCoinStakingMethods
+}
