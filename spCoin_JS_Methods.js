598aa8cd7fdc75068ea119b58e1e2d510c50362e
sponsorCoinAdmin <sponsorCoinAdmin@proton.me>
2024-11-26 04:06:05 -0500

    WIP

--- spCoin_JS_Methods.js
+++ spCoin_JS_Methods.js
@@ -1,13 +1,20 @@
-import hre  from 'hardhat'
 import { ethers }  from 'ethers'
-const { SpCoinLogger, LOG_MODE } = require("./lib/utils/logging");
-const { SpCoinERC20Methods } = require("./lib/spCoinTransferMethods");
-const { SpCoinDeleteMethods } = require("./lib/spCoinDeleteMethods");
-const { SpCoinAddMethods } = require("./lib/spCoinAddMethods");
-const { SpCoinReadMethods } = require("./lib/SpCoinReadMethods");
-const { SpCoinRewardsMethods } = require("./lib/spCoinRewardsMethods"); 
-const { SpCoinStakingMethods } = require("./lib/spCoinStakingMethods"); 
-const { second, minute, hour, day, week, year, month , millennium } = require("./lib/spCoinStakingMethods");
+// const { SpCoinLogger, LOG_MODE } = require("./lib/utils/logging");
+// const { SpCoinERC20Methods } = require("./lib/spCoinTransferMethods");
+// const { SpCoinDeleteMethods } = require("./lib/spCoinDeleteMethods");
+// const { SpCoinAddMethods } = require("./lib/spCoinAddMethods");
+// const { SpCoinReadMethods } = require("./lib/SpCoinReadMethods");
+// const { SpCoinRewardsMethods } = require("./lib/spCoinRewardsMethods"); 
+// const { SpCoinStakingMethods } = require("./lib/spCoinStakingMethods"); 
+// const { second, minute, hour, day, week, year, month , millennium } = require("./lib/spCoinStakingMethods");
+import { SpCoinLogger, LOG_MODE } from "./lib/utils/logging";
+import { SpCoinERC20Methods } from "./lib/spCoinTransferMethods";
+import { SpCoinDeleteMethods } from "./lib/spCoinDeleteMethods";
+import { SpCoinAddMethods } from "./lib/spCoinAddMethods";
+import { SpCoinReadMethods } from "./lib/SpCoinReadMethods";
+import { SpCoinRewardsMethods } from "./lib/spCoinRewardsMethods";
+import { SpCoinStakingMethods } from "./lib/spCoinStakingMethods";
+import { second, minute, hour, day, week, year, month, millennium } from "./lib/spCoinStakingMethods";
 
 class SpCoinClassMethods {
   // constructor(_spCoinContractDeployed) {
